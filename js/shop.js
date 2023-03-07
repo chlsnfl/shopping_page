f438be806ebc1ea919d92638206c57956526260f
chlsnfl <cnr1108@naver.com>
2023-03-07 18:00:50 +0900

    second commit

--- js/shop.js
+++ js/shop.js
@@ -1,17 +1,27 @@
 (function(){
     const listAll = document.getElementsByClassName('list-all')[0];
+    const cart=document.getElementById('cart');
         listAll.addEventListener("click", changeNav);
+        cart.addEventListener("click", cartBoxView);
     
     function changeNav(){
         const nav = document.getElementsByTagName('nav')[0].offsetTop+52;
-        document.getElementById('sitemap').style.top = nav+"px";
-        document.getElementById("sitemap").style.display="block";
+        const sitemap = document.getElementById('sitemap');
         listAll.classList.toggle('close');
         listAll.classList.toggle('newlist');
+        console.log(listAll.className);
+        if(listAll.className === 'close'){
+            sitemap.style.top = nav+"px";
+            sitemap.style.display="block";
+        }else{
+            sitemap.style.display="none";
         }
-    }
+        }
+        function cartBoxView(){
+            document.getElementsByClassName('cart-view')[0].classList.toggle('none');
+        }
+
+    }());
     // window.onload=function(){
     
     // }
-
-());
