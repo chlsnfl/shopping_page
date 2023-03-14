bc9c030bab40aee9e48089ed93b41ae2df0c3dd2
chlsnfl <cnr1108@naver.com>
2023-03-14 17:54:20 +0900

    add json

--- js/shop.js
+++ js/shop.js
@@ -92,3 +92,95 @@
     tabs.children[e].classList.add('active');
  }   
  
+//  fetch('data/test.json')
+   
+//   .then((res)=>res.json())
+//   .then((res)=>{
+//    let li = '';
+//    for(i=0;i<res.shoplist.length;i++){
+//       li +=
+//       '<div class="col-lg-3 my-5">'+
+//       '<div class="card">'+
+//          '<a href="#">'+
+//              '<img src="'+res.shoplist[i].img+'" class="img-fluid" alt="001">'+
+//          '</a>'+
+//           '<div class="pd-list">'+
+//               '<li>'+
+//                   '<span class="'+res.shoplist[i].color[0]+'"></span>'+
+//                   '<span class="'+res.shoplist[i].color[1]+'"></span>'+
+//                   '<span class="'+res.shoplist[i].color[2]+'"></span>'+
+//                   '<span class="'+res.shoplist[i].color[3]+'"></span>'+
+//               '</li>'+
+//               '<li class="pd-title">'+
+//                   '<h1>"'+res.shoplist[i].title+'"</h1>'+
+//               '</li>'+
+//               '<li>'+
+//                   '<del>'+res.shoplist[i].delprice+'</del>'+
+//                   '<br>'+
+//                   '<strong>'+res.shoplist[i].saleprice+'</strong>'+
+//               '</li>'+
+//               '<li>'+
+//                   '<p>'+res.shoplist[i].content+'</p>'+
+//               '</li>'+
+//               '<li class="last">'+
+//                   '<span>'+res.shoplist[i].news+'</span>'+
+//                   '<span>핫딜할인5%</span>'+
+//               '</li>'+
+
+//           '</div>'+
+//       '</div>'+
+//   '</div>'+
+// '</div>'
+//    }
+//    document.getElementById('row').innerHTML = li;
+// }
+// )
+
+
+fetch('./data/shoplist.json')
+.then((res)=>res.json())
+.then((res)=> {
+    const rs = res.shoplist; 
+    let div = "";
+
+    for(let i = 0; i < rs.length; i++){
+      div += `<div class="col-lg-3 my-5">
+        <div class="card">
+            <a href="main.html">
+              <img src="${rs[i].img}" class="img-fluid" alt="${rs[i].img}" />
+            </a>
+            <div class="card-body">
+               <ul class="pd-list">
+                  <li>`;
+
+      var color = '';   
+      for(let c=0; c < rs[i].color.length; c++){
+        color +=  `<span class="${rs[i].color[c]}"></span>`;
+      }             
+      div += color;            
+      div += `</li>
+                  <li class="pd-title">
+                      <h1>${rs[i].title}</h1>
+                  </li>
+                  <li>
+                      <del>${rs[i].delprice}</del>
+                      <br>
+                      <strong>${rs[i].saleprice}</strong>
+                  </li>
+                  <li>
+                      <p>${rs[i].content}</p>
+                  </li>
+                  <li class="last">
+                     <span>베스트셀러</span>
+                     <span>핫딜할인5%</span>
+                  </li>
+               </ul> 
+            </div>
+        </div>
+     </div>`;
+    }
+
+    document.getElementById("row").innerHTML = div;
+
+})
+.catch((err)=> console.log(err));
